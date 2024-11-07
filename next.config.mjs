6897ff0ff824aefa0a445644340efab988b21917
babuljackdev <babuljackdev@gmail.com>
2024-11-07 16:11:05 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    trailingSlash: true,
-    output: 'export',  
-    basePath: '/wibmo_pay',
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
