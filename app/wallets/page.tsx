ffb279a93a8d51a18aaa9f7ce091ef877a7bce87
babuljackdev <babuljackdev@gmail.com>
2024-11-07 22:08:14 +0600

    update-error

--- app/wallets/page.tsx
+++ app/wallets/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Bell, ChevronRight, CreditCard, DollarSign, Wallet } from 'lucide-react'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
